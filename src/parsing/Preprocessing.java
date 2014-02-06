ee628c53379fb4c98b2ff8df845a61cb9187bdda
wotann07 <nabdel07@gmail.com>
2014-02-06 10:41:43 -0500

    Created preprocessing class
    Empty class… to be developed

---
+++ src/parsing/Preprocessing.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package parsing;
+
+import java.util.*;
+
+/**
+ * @author Abdel Gomez-Perez 6201022
+ * @author Naglaa Eid 6177022
+ *
+ */
+
+public class Preprocessing {
+	Map<Integer, ArrayList<String>> dictionary;
+	
+	public Preprocessing() {
+		this.dictionary = new HashMap<Integer, ArrayList<String>>();
+	}
+}
